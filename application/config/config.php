1cf6a482536cb8d083ce734aa565263adbb81b62
Aldi Unanto <aldiunanto@yahoo.com>
2015-08-31 13:02:18 +0700

    Leave base_url empty instead

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/alamin/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
