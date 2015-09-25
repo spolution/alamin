ff09a5186adbb456a8d1f5d3416cef0475bce014
erika <myemailbuangan@gmail.com>
2015-09-25 11:13:24 +0700

    Let base_url empty

--- old/application/config/config.php
+++ old/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://aldiunanto/github/alamin/old/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
