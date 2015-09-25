05e0c4ee77f7873a43a0ba14d8e18c0157cfaeac
erika <myemailbuangan@gmail.com>
2015-09-25 11:26:52 +0700

    Adaptation of HTML and some DB's fields

--- application/libraries/access.php
+++ application/libraries/access.php
@@ -10,23 +10,23 @@
 		function login($data){
 			if ($data) {
 				$session_data = array(
-					'username'	=> $data->username,
-					'name'		=> $data->nama,
-					'is_login'	=> TRUE
+					'user_name'		=> $data->user_name,
+					'user_nickname'	=> $data->user_nickname,
+					'is_login'		=> true
 				);
 				
 				$this->CI->session->set_userdata($session_data);
 				redirect('home');
 			}else{
-				$this->CI->session->set_flashdata('error_login','<span class="error">&raquo; Kombinasi username dan password salah!</span>');
+				$this->CI->session->set_flashdata('failed', '<div class="alert alert-danger alert-dismissable">Kombinasi Username dan Password salah!</div>');
 				redirect('admin/login');
 			}
 		}
 		function logout(){
 			$data = array(
-				'username'	=> null,
-				'name'		=> null,
-				'is_login'	=> FALSE
+				'user_name'		=> null,
+				'user_nickname'	=> null,
+				'is_login'		=> false
 			);
 			
 			$this->CI->session->unset_userdata($data);
@@ -35,32 +35,9 @@
 			redirect('admin/login');
 		}
 		function is_logged_in(){
-			if ($this->CI->session->userdata('is_login') !== TRUE) {
+			if ($this->CI->session->userdata('is_login') !== true) {
 				redirect('admin/login');
-				exit();
 			}
-		}
-		function get_start_number($segment_numb){
-			if (!$this->CI->uri->segment($segment_numb)) {
-				$no = 1;
-			}else{
-				$no = $this->CI->uri->segment($segment_numb) + 1;
-			}
-			
-			return $no;
-		}
-		function now(){
-			return date('Y-m-d H:i:s');
-		}
-		function set_current_nav($param){
-			$base_nav = array(''=>'','beranda'=>'','kartu'=>'','mc'=>'','penjualan'=>'','member'=>'','lap'=>'','backup'=>'');
-			$base_nav[$param] = 'current';
-			
-			return $base_nav;
-		}
-		function add_logActivity($text){
-			$this->CI->load->database();
-			return $this->CI->db->insert('log_activity', array('text' => $text,'date'=>$this->now()));
 		}
 		
 	}
