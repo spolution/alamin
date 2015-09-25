27d441bc829f08062fb47951fe66bcd6bfec2f8f
erika <myemailbuangan@gmail.com>
2015-09-25 11:29:23 +0700

    Add some utilities function

--- application/helpers/utilities_helper.php
+++ application/helpers/utilities_helper.php
@@ -3,3 +3,15 @@
 	function assets_url($file = ''){
 		return base_url('assets/' . $file);
 	}
+	function get_start_number($segment_numb){
+		if (! $this->CI->uri->segment($segment_numb)) {
+			$no = 1;
+		}else{
+			$no = $this->CI->uri->segment($segment_numb) + 1;
+		}
+		
+		return $no;
+	}
+	function now($time = false){
+		return date('Y-m-d' . ($time ? ' H:i:s' : ''));
+	}
